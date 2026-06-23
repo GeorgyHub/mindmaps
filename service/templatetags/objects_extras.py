7cff879e0701714a02b1831f0822e52e167f653a
Georgy <83170949+GeorgyHub@users.noreply.github.com>
2026-06-23 13:08:53 +0300

    files added

---
+++ service/templatetags/objects_extras.py
@@ -0,0 +1,9 @@
+import os
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.filter(name='lookup')
+def lookup(d, key):
+	return d[key]
